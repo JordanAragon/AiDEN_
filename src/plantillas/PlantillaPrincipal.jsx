41916efa739347f88d9485bb34ed160ca06a59c7
Jordan Aragon <jordandavidaragon@outlook.com>
2026-09-15 15:07:28 -0500

    PlantillaPrincipal: animar la transición de módulo al navegar (fade + slide sutil)

--- src/plantillas/PlantillaPrincipal.jsx
+++ src/plantillas/PlantillaPrincipal.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import BarraLateral from "../components/navegacion/BarraLateral";
 import BarraSuperior from "../components/navegacion/BarraSuperior";
 
 export default function PlantillaPrincipal() {
   const [dataVersion, setDataVersion] = useState(0);
+  const { pathname } = useLocation();
 
   useEffect(() => {
     const refresh = () => setDataVersion((version) => version + 1);
@@ -23,7 +24,10 @@ export default function PlantillaPrincipal() {
       <BarraLateral />
       <section className="flex min-w-0 flex-1 flex-col overflow-hidden">
         <BarraSuperior />
-        <main key={dataVersion} className="min-w-0 flex-1 overflow-y-auto p-4 sm:p-6">
+        <main
+          key={`${pathname}-${dataVersion}`}
+          className="aiden-transicion-modulo min-w-0 flex-1 overflow-y-auto p-4 sm:p-6"
+        >
           <Outlet />
         </main>
       </section>
