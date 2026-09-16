dffb75f2b0940494005d276e2d9bbd7a3441fe07
Jordan Aragon <jordandavidaragon@outlook.com>
2026-09-16 17:18:22 -0500

    ui: integrar sistema visual transversal en la app

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./estilos/index.css";
 import "./estilos/modo-oscuro.css";
 import "./estilos/animaciones-app.css";
+import "./estilos/experiencia-aiden.css";
 import App from "./App.jsx";
 
 const temaGuardado = localStorage.getItem("aiden-theme");
